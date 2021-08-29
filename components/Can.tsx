35b1c039e404ae1adbb0e226bd34d3aaa663dd56
Luís Henrique Machado <luis.machado@ambevtech.com.br>
2021-08-29 10:58:15 -0300

    Finalizando código

---
+++ components/Can.tsx
@@ -0,0 +1,21 @@
+import { Children, ReactNode } from "react";
+import { useCan } from "../hooks/useCan";
+
+interface CanProps {
+  children: ReactNode;
+  permissions?: string[];
+  roles?: string[];
+}
+
+export function Can({ children, permissions, roles }: CanProps) {
+  const userCanSeeComponent = useCan({
+    permissions,
+    roles,
+  });
+
+  if (!userCanSeeComponent) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
